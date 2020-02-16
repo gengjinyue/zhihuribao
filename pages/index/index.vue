cabba827c3e7dc3233aff961cadb724613d92809
张春雨 <zhangchunyu@zhangchunyudeMacBook-Pro.local>
2020-02-16 16:00:06 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,204 @@
+<template>
+	<view class="content">
+		<view class="text-nav">
+			<view class="navLeft">
+				111
+			</view>
+			<text class="space">|</text>
+			<text class="navTitle">知乎日报</text>
+			<view class="navRight">
+				<navigator url="../people/people" open-type="navigate"><uni-icons type="contact" size="30"></uni-icons></navigator>
+			</view>
+		</view>
+			<!-- <view class="uni-padding-wrap">
+				<view class="page-section swiper">
+					<view class="page-section-spacing">
+						<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+							<swiper-item>
+								<view class="swiper-item uni-bg-red">A</view>
+							</swiper-item>
+							<swiper-item>
+								<view class="swiper-item uni-bg-green">B</view>
+							</swiper-item>
+							<swiper-item>
+								<view class="swiper-item uni-bg-blue">C</view>
+							</swiper-item>
+						</swiper>
+					</view>
+				</view>
+			</view> -->
+			<view class="banner">
+				<swiper class="swiper" indicator-color="rgba(255, 255, 255, 0.6)" indicator-active-color="rgba(255, 255, 255, 1)"
+				 :autoplay="true" :interval="3000" :duration="1000" :current="swiperCurrent" @change="changeSwiper">
+					<swiper-item v-for="item in swiperImg" :key="item.id">
+						<image class="swiper-item" :src="item" mode="widthFix" style="height:600rpx;"></image>
+					</swiper-item>
+				</swiper>
+				<view class="dots">
+					<block v-for="(item,index) in swiperImg.length" :key="item.id">
+						<view class="dot" :class="index==swiperCurrent ? ' active' : ''"></view>
+					</block>
+				</view>
+			</view>
+		<view class="indexList" >
+			<view class="fistList">
+				<view class="listLeft">
+					<text class="leftTile">明明很多中国人皮肤不算黄色？</text>
+					<text class="leftText"> 情报员 3分钟</text>
+				</view>
+				<view class="listRt">
+					<image src="../../static/logo.png" mode="" ></image>
+				</view>
+			</view>
+			<view class="fistList">
+				<view class="listLeft">
+					<text class="leftTile">明明很多中国人皮肤不算黄色？</text>
+					<text class="leftText"> 情报员 3分钟</text>
+				</view>
+				<view class="listRt">
+					<image src="../../static/logo.png" mode="" ></image>
+				</view>
+			</view>
+			<view class="fistList">
+				<view class="listLeft">
+					<text class="leftTile">明明很多中国人皮肤不算黄色？</text>
+					<text class="leftText"> 情报员 3分钟</text>
+				</view>
+				<view class="listRt">
+					<image src="../../static/logo.png" mode="" ></image>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniIcons from "@/components/uni-icons/uni-icons.vue"
+	export default {
+		components: {uniIcons},
+		data() {
+			return {
+				// data:new Date().toISOString().slice(0, 10),
+				// background: [],
+				// indicatorDots: true,
+				// autoplay: true,
+				// interval: 2000,
+				// duration: 500
+			swiperImg: [
+				'../../static/swiper.png',					
+				'../../static/swiper1.png',
+				'../../static/swiper.png'
+			],
+			none: true,
+			current: 0,
+			mode: 'default',
+			swiperCurrent: 0,
+			}
+			},
+		onLoad() {
+
+		},
+		methods: {
+		    changeSwiper(e) {
+		    	this.swiperCurrent = e.detail.current;
+		    }
+		 }
+	}
+</script>
+<style lang="scss">
+	.content {
+		.text-nav{
+			/* background-color: red; */
+			height: 100rpx;
+		}
+		.space{
+			padding: 0 10rpx;
+			color: #999;
+			line-height: 100rpx;
+		}
+		.navTitle{
+			font-size: 40rpx;
+		}
+		.navLeft{
+			float: left;
+			width: 10%;
+			font-size: 30rpx;
+			line-height: 100rpx;
+		}
+		.navRight{
+			float: right;
+			width: 10%;
+			line-height: 100rpx;
+		}
+		.banner{
+			.swiper {
+				width: 100%;
+				height: 500rpx;
+				border-radius: 16rpx;
+				overflow: hidden;
+				margin-top: 16rpx;
+				position: relative;
+				background: yellow;
+				.swiper-item {
+					width: 100%;
+					height: 500rpx;
+				}
+			}
+			.dots {
+				position: absolute;
+				top: 600rpx;
+				left: 80%;
+				transform: translate(-50% 0);
+				z-index: 4;
+				display: flex;
+				justify-content: center;
+			
+				.dot {
+					width: 24rpx;
+					height: 8rpx;
+					transition: all .6s;
+					background: rgba(0, 0, 0, .3);
+					margin-right: 10rpx;
+				}
+			
+				.active {
+					width: 24rpx;
+					height: 8rpx;
+					background: rgba(255, 255, 255, .8);
+				}
+			}
+		}
+		.indexList{
+			margin-top: 20rpx;
+			overflow: hidden;
+			padding-left: 30rpx;
+			.fistList{
+				height: 200rpx;
+				overflow: hidden;
+				.listLeft{
+					float: left;
+					width: 77%;
+					.leftTile{
+						padding-top: 20rpx;
+						font-size: 40rpx;
+						display: block;
+					}
+					.leftText{
+						font-size: 30rpx;
+						color: #C0C0C0;
+					}
+				}
+				.listRt{
+					float: right;
+					width: 20%;
+					line-height: 200rpx;
+					image{
+						height: 120rpx;
+						width: 120rpx;
+						vertical-align: middle;
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ pages/people/people.vue
@@ -0,0 +1,24 @@
+<template>
+		<view>		<view class="topTab"></view>		<view class="goback">			<img src="" />		</view>		<view class="cent">			<view class="mimages"></view>			<view class="mname">不足为害</view>		</view>		<view class="mcollect">			<view class="mycoll">我的收藏</view>			<view class="mright"><uni-icons type="arrowright" size="20"></uni-icons></view>		</view>		<view class="mcollect">			<view class="mycoll">消息中心</view>			<view class="mright"><uni-icons type="arrowright" size="20"></uni-icons></view>		</view>		<view class="myTab">			<view>				<view><uni-icons type="eye" size="40"></uni-icons></view>				<view>夜间模式</view>			</view>			<view>				<view><a herf="#"><uni-icons type="gear" size="40"></uni-icons></a></view>				<view>设置</view>			</view>		</view>	</view>
+</template>
+
+<script>
+	import uniIcons from "@/components/uni-icons/uni-icons.vue"
+	export default {
+		components: {uniIcons},
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+	
+		},
+		methods: {
+		 }
+}
+</script>
+
+<style>
+	.topTab{		height:20px;		width: 100%;	}	.goback{		padding:0 15px ;		width: 100%;		height: 30px;		text-align: left;	}	.goback img{		width: 25px;	}	.mimages{		width: 80px;		height: 80px;		border-radius: 50px;		background-color: #42B983;		margin: 0 auto;		/* display: none; */	}	.cent{		width: 100%;		/* background-color: #FFAABB; */		margin-bottom: 20px;	}	.mname{		width: 100%;		margin: 0 auto;		font-weight: bold;		line-height: 40px;		font-size: 20px;
+		text-align: center;	}	.mcollect{		border-top: 1px solid #F6F6F6;		display: flex;		padding: 15px 10px;			}	.mycoll{		font-size: 18px;			}	.mright{		position: absolute;		right: 20px;	}	.myTab{		display: flex;		justify-content:space-around;		position: absolute;		bottom: 40px;		width: 100%;	}
+</style>
